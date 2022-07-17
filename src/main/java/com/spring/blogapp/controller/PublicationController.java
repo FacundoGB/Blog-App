e54516c6a7e451fedbc8b96e896644d4f80f26d5
github_FacundoGB <bardifacundo@gmail.com>
2022-07-17 20:06:38 -0300

    deployed sorting in controller. checked in postman

--- src/main/java/com/spring/blogapp/controller/PublicationController.java
+++ src/main/java/com/spring/blogapp/controller/PublicationController.java
@@ -1,7 +1,7 @@
 package com.spring.blogapp.controller;
 
 import com.spring.blogapp.dto.PublicationDto;
-import com.spring.blogapp.entity.Publication;
+import com.spring.blogapp.dto.PublicationResponse;
 import com.spring.blogapp.service.PublicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,20 +17,23 @@ public class PublicationController {
     @Autowired
     PublicationService service;
 
-    @PostMapping
-    public ResponseEntity<PublicationDto> savePublication(@RequestBody PublicationDto publicDto){
-        return  new ResponseEntity<>(service.createPublication(publicDto), HttpStatus.CREATED);
-
-    }
-
     @GetMapping
-    public List<PublicationDto> listAllPublications() {
-        return service.listAllPublications();
+    public PublicationResponse listAllPublications(@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageN,
+                                                   @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageS,
+                                                   @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
+                                                   @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+        return service.listAllPublications(pageN,pageS,sortBy,sortDir);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PublicationDto> obtainPublicationById(@PathVariable(name = "id") long id) {
         return ResponseEntity.ok(service.showPublicationById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<PublicationDto> savePublication(@RequestBody PublicationDto publicDto){
+        return  new ResponseEntity<>(service.createPublication(publicDto), HttpStatus.CREATED);
+
     }
 
     @PutMapping("/{id}")
