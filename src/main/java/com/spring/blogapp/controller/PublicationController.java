8cdaab76e70e91312634a18f88b3945ec8d8c19e
github_FacundoGB <bardifacundo@gmail.com>
2022-07-17 17:53:14 -0300

    made controller methods for CRUD

--- src/main/java/com/spring/blogapp/controller/PublicationController.java
+++ src/main/java/com/spring/blogapp/controller/PublicationController.java
@@ -1,14 +1,14 @@
 package com.spring.blogapp.controller;
 
 import com.spring.blogapp.dto.PublicationDto;
+import com.spring.blogapp.entity.Publication;
 import com.spring.blogapp.service.PublicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/publications")
@@ -22,4 +22,29 @@ public class PublicationController {
         return  new ResponseEntity<>(service.createPublication(publicDto), HttpStatus.CREATED);
 
     }
+
+    @GetMapping
+    public List<PublicationDto> listAllPublications() {
+        return service.listAllPublications();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PublicationDto> obtainPublicationById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(service.showPublicationById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PublicationDto> modifyPublication(@RequestBody PublicationDto publicDto, @PathVariable(name = "id") long id) {
+        PublicationDto response = service.modifyPublication(publicDto, id);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePublication(@PathVariable(name = "id")long id) {
+        service.deletePublication(id);
+        return new ResponseEntity<>("Publication deleted successfully!", HttpStatus.OK);
+    }
 }
+
+
