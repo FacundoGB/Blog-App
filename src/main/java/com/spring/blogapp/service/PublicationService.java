dfb6fbe93a06b3565e027210478d4e6ea7ee76e5
github_FacundoGB <bardifacundo@gmail.com>
2022-07-17 17:51:37 -0300

    created methods of deletion, modification, list all and find by id to be implemented in PublicationServiceImp

--- src/main/java/com/spring/blogapp/service/PublicationService.java
+++ src/main/java/com/spring/blogapp/service/PublicationService.java
@@ -2,8 +2,17 @@ package com.spring.blogapp.service;
 
 import com.spring.blogapp.dto.PublicationDto;
 
+import java.util.List;
+
 public interface PublicationService {
 
-    public PublicationDto createPublication(PublicationDto publicationDTO);
+    public PublicationDto createPublication(PublicationDto dtopublication);
 
+    public List<PublicationDto> listAllPublications();
+
+    public PublicationDto showPublicationById(long id);
+
+    public PublicationDto modifyPublication(PublicationDto dtopublication, long id);
+
+    public void deletePublication (long id);
 }
