4ab56acfafa47a426b7ac8a8f6f32db78011be47
github_FacundoGB <bardifacundo@gmail.com>
2022-07-17 20:05:32 -0300

    added sorting capabilities both by id and by order to be implemented in srvice implementation

--- src/main/java/com/spring/blogapp/service/PublicationService.java
+++ src/main/java/com/spring/blogapp/service/PublicationService.java
@@ -1,18 +1,19 @@
 package com.spring.blogapp.service;
 
 import com.spring.blogapp.dto.PublicationDto;
+import com.spring.blogapp.dto.PublicationResponse;
 
 import java.util.List;
 
 public interface PublicationService {
 
-    public PublicationDto createPublication(PublicationDto dtopublication);
+    public PublicationDto createPublication(PublicationDto publicationDto);
 
-    public List<PublicationDto> listAllPublications();
+    public PublicationResponse listAllPublications(int pageN, int pageS, String sortBy, String sortDir);
 
     public PublicationDto showPublicationById(long id);
 
-    public PublicationDto modifyPublication(PublicationDto dtopublication, long id);
+    public PublicationDto modifyPublication(PublicationDto publicationDto, long id);
 
     public void deletePublication (long id);
 }
