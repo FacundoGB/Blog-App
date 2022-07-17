0f1dde2b6c79744c8d97c98f9e1d252096d0f5e1
github_FacundoGB <bardifacundo@gmail.com>
2022-07-17 15:30:48 -0300

    created publciation service inteface for method used in implementation. First creation method

---
+++ src/main/java/com/spring/blogapp/service/PublicationService.java
@@ -0,0 +1,9 @@
+package com.spring.blogapp.service;
+
+import com.spring.blogapp.dto.PublicationDto;
+
+public interface PublicationService {
+
+    public PublicationDto createPublication(PublicationDto publicationDTO);
+
+}
