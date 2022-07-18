fda31415114ed0ff207d86e48211f4c7fb1543ce
github_FacundoGB <bardifacundo@gmail.com>
2022-07-18 17:04:46 -0300

    added validation for Comment

--- src/main/java/com/spring/blogapp/dto/CommentDto.java
+++ src/main/java/com/spring/blogapp/dto/CommentDto.java
@@ -4,11 +4,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter@Setter@NoArgsConstructor
 public class CommentDto {
 
-    private long id;
+    private Long id;
+
+    @NotEmpty(message = "Name should not be empty or null")
     private String name;
+
+    @NotEmpty(message = "Mail should not be empty or null")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Mail should not be empty or null")
+    @Size(min = 10, message = "body should not be empty or null, and must be at least 10 characters long")
     private String body;
 }
