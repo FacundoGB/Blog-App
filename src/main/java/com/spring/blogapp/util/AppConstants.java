476dbbe5666c74e3361d1e907120dc86e2f9eeab
github_FacundoGB <bardifacundo@gmail.com>
2022-07-17 21:19:23 -0300

    created constants used in PublicationController to manage default values in url

---
+++ src/main/java/com/spring/blogapp/util/AppConstants.java
@@ -0,0 +1,9 @@
+package com.spring.blogapp.util;
+
+public class AppConstants {
+
+    public static final String NUMBER_OF_PAGE_DEFAULT = "0";
+    public static final String SIZE_OF_PAGE_DEFAULT = "10";
+    public static final String ORDER_BY_DEFECT_DEFAULT = "id";
+    public static final String ORDER_BY_DIRECTION_DEFAULT = "asc";
+}
