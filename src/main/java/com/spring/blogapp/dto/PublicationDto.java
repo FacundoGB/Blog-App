f8ee3be25d37937ea1b356857290b194dc5dcd7e
github_FacundoGB <bardifacundo@gmail.com>
2022-07-18 16:52:20 -0300

    added validation to entries for the publication

--- src/main/java/com/spring/blogapp/dto/PublicationDto.java
+++ src/main/java/com/spring/blogapp/dto/PublicationDto.java
@@ -3,6 +3,8 @@ package com.spring.blogapp.dto;
 import com.spring.blogapp.entity.Comment;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Getter @Setter @ToString
@@ -10,9 +12,19 @@ import java.util.Set;
 @NoArgsConstructor
 public class PublicationDto {
 
+
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Publications title is too small. Use at least 3 letters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Publications description is too small. Use at least 3 letters")
     private String description;
+
+    @NotEmpty
     private String content;
+
     private Set<Comment> comments;
 }
