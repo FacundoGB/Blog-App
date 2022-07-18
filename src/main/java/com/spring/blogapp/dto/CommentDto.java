ea312e2e58131f7eed9eefa68389b80081fdd678
github_FacundoGB <bardifacundo@gmail.com>
2022-07-17 22:56:31 -0300

    created dto for comment

---
+++ src/main/java/com/spring/blogapp/dto/CommentDto.java
@@ -0,0 +1,14 @@
+package com.spring.blogapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter@NoArgsConstructor
+public class CommentDto {
+
+    private long id;
+    private String name;
+    private String email;
+    private String body;
+}
