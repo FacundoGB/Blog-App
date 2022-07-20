f8bae8f81fbb6f347ca4acf1b09ca4ef131adbe2
github_FacundoGB <bardifacundo@gmail.com>
2022-07-20 13:14:08 -0300

    created registration data transfer object

---
+++ src/main/java/com/spring/blogapp/dto/RegisterDto.java
@@ -0,0 +1,13 @@
+package com.spring.blogapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class RegisterDto {
+
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+}
