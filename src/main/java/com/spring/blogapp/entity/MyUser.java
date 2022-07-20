45a1f44bedc5b6c666f814fce03c0a6c70fc0fc2
github_FacundoGB <bardifacundo@gmail.com>
2022-07-19 23:59:43 -0300

    created user entity

---
+++ src/main/java/com/spring/blogapp/entity/MyUser.java
@@ -0,0 +1,37 @@
+package com.spring.blogapp.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"username"}),
+         @UniqueConstraint(columnNames = {"email"})})
+@Getter@Setter
+@NoArgsConstructor
+public class MyUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public MyUser(String email, String password, Collection<? extends GrantedAuthority> mapRoles) {
+    }
+}
